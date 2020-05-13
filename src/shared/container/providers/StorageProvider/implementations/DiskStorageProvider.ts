1ea31b968955a5acb46aad59329f3d4cfff47d90
PRAZERES SANTOS Ilton Karly <ikprazeressantos@europe.corp.altran.com>
2020-05-13 16:51:28 +0100

    Finalizando testes

---
+++ src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import UploadConfig from '@config/upload';
+import IStorageProvider from '../models/IStorageProvider';
+
+class DiskStorageProvider implements IStorageProvider {
+  public async saveFile(file: string): Promise<string> {
+    await fs.promises.rename(
+      path.resolve(UploadConfig.tmpFolder, file),
+      path.resolve(UploadConfig.uploadFolder, file),
+    );
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    const filePath = path.resolve(UploadConfig.uploadFolder, file);
+
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+    await fs.promises.unlink(filePath);
+  }
+}
+
+export default DiskStorageProvider;
