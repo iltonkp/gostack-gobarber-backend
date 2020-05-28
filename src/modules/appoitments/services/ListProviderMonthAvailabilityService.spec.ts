9d7dceed482a5b23c10ccec00c4070d5b68cc6ee
PRAZERES SANTOS Ilton Karly <iltonk.si@gmail.com>
2020-05-28 17:38:21 +0100

    Listando dias disponíveis

--- src/modules/appoitments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appoitments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -20,7 +20,46 @@ describe('ListProviderMonthAvailabilityService', () => {
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      date: new Date(2020, 4, 28, 9, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
       date: new Date(2020, 4, 28, 10, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 28, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 28, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 28, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 28, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 28, 15, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 28, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 28, 17, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
@@ -39,7 +78,7 @@ describe('ListProviderMonthAvailabilityService', () => {
         { day: 26, available: true },
         { day: 27, available: true },
         { day: 28, available: false },
-        { day: 29, available: false },
+        { day: 29, available: true },
       ]),
     );
   });
