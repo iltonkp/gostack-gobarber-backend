75610d8b304e528e2568e9b28cebfa1bd84f1f42
PRAZERES SANTOS Ilton Karly <iltonk.si@gmail.com>
2020-05-29 15:12:40 +0100

    Regras de agendamento

--- src/modules/appoitments/services/CreateAppointmentService.ts
+++ src/modules/appoitments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -25,6 +25,21 @@ class CreateAppointmentService {
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
+    if (provider_id === user_id) {
+      throw new AppError('Você não pode realizar um agendamento consigo mesmo');
+    }
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError(
+        'Você não pode realizar um agendamento para uma data no passado',
+      );
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'Você só pode realizar um agendamentos entre 8 horas e 17 horas',
+      );
+    }
     const findAppointementInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
