7a8b2efc8c6ff561da896113c159370d60e97167
PRAZERES SANTOS Ilton Karly <iltonk.si@gmail.com>
2020-06-02 11:28:06 +0100

    Invalidando cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,11 +1,11 @@
 import { injectable, inject } from 'tsyringe';
 
+import AppError from '@shared/errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
+import ICaheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
-import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
-
-import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
   name: string;
@@ -19,6 +19,8 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+    @inject('CacheProvider')
+    private cacheProvider: ICaheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -34,6 +36,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
